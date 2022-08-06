c095c57a75bc817c49490c73857a2ff92c92fea6
nikolas-poberezhniy <nikolas.poberezhniy@gmail.com>
2022-08-06 21:21:52 +0300

    done 1st task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Дополнительный импорт стилей
+// Change code below this line
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+function createItemsMarcup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item"  href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+
+function populateGallery(markup) {
+  refs.gallery.innerHTML = markup;
+}
+
+const itemsMarcup = createItemsMarcup(galleryItems);
+
+populateGallery(itemsMarcup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: '250',
+});
+gallery.on('show.simplelightbox');
 console.log(galleryItems);
