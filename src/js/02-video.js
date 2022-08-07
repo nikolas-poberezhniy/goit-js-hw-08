cc502bcf0f46029de298aec415270d06a72e3c61
nikolas-poberezhniy <nikolas.poberezhniy@gmail.com>
2022-08-07 14:38:05 +0300

    add vimeo & throttle library + done 2nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+if (!localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(0);
+} else {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+player.on(
+  'timeupdate',
+  throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+      console.log(seconds);
+    });
+  }, 1000)
+);
